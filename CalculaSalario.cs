e0195ccfa966ab5d3cb7a1beb0b512e0ebf07ec2
Matheus Vieira <108036687+MatheusVieira2001@users.noreply.github.com>
2022-10-16 13:13:52 -0300

    Update CalculaSalario.cs

--- CalculaSalario.cs
+++ CalculaSalario.cs
@@ -3,11 +3,15 @@ using System.Diagnostics;
 using System.Globalization;
 
 namespace CalculoSalarioT
+{
     internal class CalculaSalario
     {
         static void Main(string[] args)
         {
-
+//Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+//hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+//decimais.
+            
             Console.WriteLine("Digite o NUMERO DO FUNCIONARIO: ");
             int numero = int.Parse(Console.ReadLine());
 
