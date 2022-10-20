7b4b472aa687a56d7973ccf133be149b1915c261
Matheus Vieira <108036687+MatheusVieira2001@users.noreply.github.com>
2022-10-20 20:50:22 -0300

    Update FormulaHeron.cs

--- FormulaHeron.cs
+++ FormulaHeron.cs
@@ -1,4 +1,4 @@
-using Classes;
+
 using System;
 using System.Globalization;
 
@@ -12,18 +12,21 @@ namespace Course
             x = new Triangulo();
             y = new Triangulo();
 
-            Console.WriteLine("Entre com as medidas do triângulo X:");
+            Console.WriteLine("Entre com as 3 medidas do triângulo X:");
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            
+            Console.WriteLine("Entre com as 3 medidas do triângulo Y:");
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
             if (areaX > areaY)
@@ -41,13 +44,17 @@ namespace Course
             }
 
         }
-    }
-    
-    class Triangulo
-    {
-        public double A;
-        public double B;
-        public double C;
-
+        
+        class Triangulo
+        {
+            public double A;
+            public double B;
+            public double C;
+            public double Area()
+            {
+                double p = (A + B + C) / 2.0;
+                return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            }
+        }
     }
 }
