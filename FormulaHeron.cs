1373dff44cd9cd32d32d09c12577929d11676b91
Matheus Vieira <108036687+MatheusVieira2001@users.noreply.github.com>
2022-10-20 20:12:20 -0300

    Update FormulaHeron.cs

--- FormulaHeron.cs
+++ FormulaHeron.cs
@@ -1,29 +1,29 @@
+using Classes;
 using System;
-using System.Diagnostics;
 using System.Globalization;
 
-
-namespace AREA
+namespace Course
 {
-    internal class FormulaHeron
+    class Program
     {
         static void Main(string[] args)
         {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
 
-
-            double xA, xB, xC, yA, yB, yC;
-            Console.WriteLine("Entre com as 3 medidas do triângulo X:");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Entre com as  3 medidas do triângulo Y:");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-            p = (yA + yB + yC) / 2.0;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
             if (areaX > areaY)
@@ -37,8 +37,17 @@ namespace AREA
             else
             {
                 Console.WriteLine("AREAS IGUAIS");
+
             }
+
         }
+    }
+    
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
 
     }
 }
